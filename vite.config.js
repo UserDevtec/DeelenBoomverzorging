75c216ce2a7aec7cf0e2f7d983ebf252d3f1ecaa
UserDevtec <info@devtec.nl>
2026-05-03 18:02:50 +0200

    url update

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import process from 'node:process'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.GITHUB_PAGES === 'true' ? '/DeelenBoomverzorging/' : '/',
+  base: '/DeelenBoomverzorging/',
   plugins: [react()],
 })
